64bba417da6b813cfa58151c2b75fc9ce616d416
vadim_cpp <ieti25@yandex.ru>
2025-08-30 17:10:14 +0500

    Добавлен скрипт для сбоки тестов

---
+++ tests/build.bat
@@ -0,0 +1,51 @@
+@echo off
+setlocal
+
+set "BUILD_DIR=build_tests"
+
+:: Установка значения флага по умолчанию
+set "CONFIGURATION=Release"
+
+:: Проверка аргументов командной строки для изменения значения флага
+for %%A in (%*) do (
+    if "%%~A"=="config--Release" (
+        set "CONFIGURATION=Release"
+    ) else if "%%~A"=="config--Debug" (
+        set "CONFIGURATION=Debug"
+    )
+)
+
+if exist "%BUILD_DIR%" (
+    echo.
+    echo Deleting a directory %BUILD_DIR%...
+    rmdir /s /q "%BUILD_DIR%"
+)
+
+echo.
+echo Creating a directory %BUILD_DIR%...
+mkdir "%BUILD_DIR%"
+
+echo.
+echo Going to the directory %BUILD_DIR%...
+cd "%BUILD_DIR%"
+
+echo.
+echo CMake execution...
+cmake ..
+
+echo.
+echo Project assembly in %CONFIGURATION% configuration...
+cmake --build . --config %CONFIGURATION%
+
+:: Проверка кода возврата сборки
+echo.
+if %ERRORLEVEL% equ 0 (
+    echo The project has been successfully assembled.
+) else (
+    echo The project assembly failed with error code %ERRORLEVEL%.
+)
+
+:: Ожидание нажатия клавиши перед закрытием консоли
+pause
+
+endlocal
